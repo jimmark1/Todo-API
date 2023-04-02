65bd7b7f25d5dab08b54d88c00137cf0c1c8eb2b
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-04-02 12:07:11 +0800

    Create urls.py

---
+++ todo_api/accounts_manager/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . views import *
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='registration'),
+    path('user-details/', RetrieveUserView.as_view(), name='user-details'),
+
+    #SIMPLE JWT AUTHENTICATION
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('forgot-password/', ForgotPasswordView.as_view()),
+    path('reset-password/<str:token>/', ResetPasswordView.as_view()),
+]
