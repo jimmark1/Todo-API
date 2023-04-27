a1557782ec03db24c9242f79e3069fe1e3ec4b9f
Jimmark Daroy <111548265+jimmark1@users.noreply.github.com>
2023-04-27 11:01:48 +0800

    Update scheduler.py

--- todo_api/tasks_manager/scheduler.py
+++ todo_api/tasks_manager/scheduler.py
@@ -2,4 +2,4 @@ from django.db import connection
 
 def delete_old_data():
     with connection.cursor() as cursor:
-        cursor.execute("DELETE FROM tasks_manager_tasks WHERE created_at < DATE('now', '-3 day')")
+        cursor.execute(" DELETE FROM tasks_manager_tasks WHERE created_at < DATE('now', '-3 day') AND task_status = 'Completed'")
